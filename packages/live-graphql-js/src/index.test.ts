0139110d4a5061cd75f1338cec34e0d5ce82de35
Eloy Durán <eloy.de.enige@gmail.com>
2021-09-22 15:33:20 +0200

    stash

--- packages/live-graphql-js/src/index.test.ts
+++ packages/live-graphql-js/src/index.test.ts
@@ -1,7 +1,51 @@
-import { bar } from ".";
+import {
+  execute,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  parse,
+} from "graphql";
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: {
+      rootField: {
+        type: GraphQLString,
+        resolve: () => {
+          const iter: AsyncIterable<number> = {
+            [Symbol.asyncIterator]() {
+              let current = 1;
+              const last = 5;
+              return {
+                async next() {
+                  await new Promise((resolve) => setTimeout(resolve, 100));
+                  if (current <= last) {
+                    return { done: false, value: current++ };
+                  } else {
+                    return { done: true, value: undefined };
+                  }
+                },
+              };
+            },
+          };
+          return iter;
+        },
+      },
+    },
+  }),
+});
 
 describe("bar", () => {
-  it("returns a string", () => {
-    expect(typeof bar("test")).toBe("string");
+  it("returns a string", async () => {
+    const result = await execute({
+      schema,
+      document: parse(`
+        query {
+          rootField
+        }
+      `),
+    });
+    console.log(result);
   });
 });
