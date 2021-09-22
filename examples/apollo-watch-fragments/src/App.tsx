9337b7513f9bff861af8b6ef321efd918237d7ed
Eloy Durán <eloy.de.enige@gmail.com>
2021-09-22 16:54:10 +0200

    Bootstrap TodoMVC html/css

--- examples/apollo-watch-fragments/src/App.tsx
+++ examples/apollo-watch-fragments/src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+        />
       </header>
-    </div>
+      {/* <!-- This section should be hidden by default and shown when there are todos --> */}
+      <section className="main">
+        <input id="toggle-all" className="toggle-all" type="checkbox" />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+        <ul className="todo-list">
+          {/* <!-- These are here just to show the structure of the list items --> */}
+          {/* <!-- List items should get the class `editing` when editing and `completed` when marked as completed --> */}
+          <li className="completed">
+            <div className="view">
+              <input className="toggle" type="checkbox" checked />
+              <label>Taste JavaScript</label>
+              <button className="destroy"></button>
+            </div>
+            <input className="edit" value="Create a TodoMVC template" />
+          </li>
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>Buy a unicorn</label>
+              <button className="destroy"></button>
+            </div>
+            <input className="edit" value="Rule the web" />
+          </li>
+        </ul>
+      </section>
+      {/* <!-- This footer should be hidden by default and shown when there are todos --> */}
+      <footer className="footer">
+        {/* <!-- This should be `0 items left` by default --> */}
+        <span className="todo-count">
+          <strong>0</strong> item left
+        </span>
+        {/* <!-- Remove this if you don't implement routing --> */}
+        <ul className="filters">
+          <li>
+            <a className="selected" href="#/">
+              All
+            </a>
+          </li>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+        {/* <!-- Hidden if no completed items are left ↓ --> */}
+        <button className="clear-completed">Clear completed</button>
+      </footer>
+    </section>
   );
-}
+};
 
 export default App;
