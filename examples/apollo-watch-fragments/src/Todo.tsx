131200f9ce572bd069771b611944e696a2eec449
Eloy Durán <eloy.de.enige@gmail.com>
2021-09-27 19:17:25 +0200

    Manually apply transform on watched node component

--- examples/apollo-watch-fragments/src/Todo.tsx
+++ examples/apollo-watch-fragments/src/Todo.tsx
@@ -2,8 +2,16 @@ import React, { useCallback } from "react";
 import { useFragment } from "@graphitation/apollo-react-relay-duct-tape";
 import { graphql } from "@graphitation/graphql-js-tag";
 
-import { Todo_todoFragment$key } from "./__generated__/Todo_todoFragment.graphql";
 import useChangeTodoStatusMutation from "./useChangeTodoStatusMutation";
+
+import {
+  Todo_todoFragment$key,
+  Todo_todoFragment as Todo_todoFragmentType,
+} from "./__generated__/Todo_todoFragment.graphql";
+
+// TODO: This needs to be done by a webpack loader:
+import { useQuery as useApolloQuery } from "@apollo/client";
+import { documentNode as watchNodeQuery } from "./__generated__/Todo_todoWatchNodeQuery.graphql";
 
 export const Todo_todoFragment = graphql`
   fragment Todo_todoFragment on Todo @watchNode {
@@ -16,7 +24,13 @@ export const Todo_todoFragment = graphql`
 export const Todo: React.FC<{ todo: Todo_todoFragment$key }> = ({
   todo: todoRef,
 }) => {
-  const todo = useFragment(Todo_todoFragment, todoRef);
+  // TODO: This needs to be replaced by the webpack loader
+  // const todo = useFragment(Todo_todoFragment, todoRef);
+  const response = useApolloQuery(watchNodeQuery as any, {
+    variables: { id: (todoRef as any).id },
+    fetchPolicy: "cache-only",
+  });
+  const todo = (response.data!.node as any) as Todo_todoFragmentType;
 
   const [changeTodoStatus] = useChangeTodoStatusMutation();
   const handleCompleteChange = useCallback(
