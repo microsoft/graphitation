76555c85944c19a4ce70f379415e98aeb4adffc7
Eloy Durán <eloy.de.enige@gmail.com>
2021-10-06 20:44:56 +0200

    Do not export build-time only code from index files
    As they'll get pulled into runtime code because we use CommonJS [in TMP] and that leads to undesired behaviour.

--- packages/apollo-react-relay-duct-tape/src/storeObservation/index.ts
+++ packages/apollo-react-relay-duct-tape/src/storeObservation/index.ts
@@ -1,3 +1,4 @@
-export * from "./createImportDocumentsTransform";
+// NOTE: Because we use CommonJS [in TMP], exports are not tree shaken and thus we can't
+//       export things here that should only be used in build-time tooling.
 export * from "./createNodeFromCacheFieldPolicy";
 export * from "./shallowCompareFragmentReferences";
