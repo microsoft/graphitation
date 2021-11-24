3ac43a9045a19e356cbec022967f6c65ced8d1ec
Jakub Vejr <jakubvejr@microsoft.com>
2021-11-24 10:23:59 +0100

    Import require-id-when-available eslint rule
    at commit https://github.com/dotansimha/graphql-eslint/tree/300f73be802bdd06432a5df34939521d1ce0d93b

---
+++ packages/graphql-eslint-rules/src/__tests__/require-id-when-available.spec.ts
@@ -0,0 +1,86 @@
+/*
+ * Taken from https://github.com/dotansimha/graphql-eslint/blob/300f73be802bdd06432a5df34939521d1ce0d93b/packages/plugin/tests/require-id-when-available.spec.ts
+ * MIT license https://github.com/dotansimha/graphql-eslint/blob/300f73be802bdd06432a5df34939521d1ce0d93b/LICENSE
+ */
+
+import { GraphQLRuleTester } from "../src/testkit";
+import rule from "../src/rules/require-id-when-available";
+import { ParserOptions } from "../src/types";
+
+const TEST_SCHEMA = /* GraphQL */ `
+  type Query {
+    hasId: HasId!
+    noId: NoId!
+    vehicles: [Vehicle!]!
+    flying: [Flying!]!
+  }
+  type NoId {
+    name: String!
+  }
+  interface Vehicle {
+    id: ID!
+  }
+  type Car implements Vehicle {
+    id: ID!
+    mileage: Int
+  }
+  interface Flying {
+    hasWings: Boolean!
+  }
+  type Bird implements Flying {
+    id: ID!
+    hasWings: Boolean!
+  }
+  type HasId {
+    id: ID!
+    name: String!
+  }
+`;
+
+const WITH_SCHEMA = {
+  parserOptions: <ParserOptions>{
+    schema: TEST_SCHEMA,
+    operations: [
+      `fragment HasIdFields on HasId {
+        id
+      }`,
+    ],
+  },
+};
+const ruleTester = new GraphQLRuleTester();
+
+ruleTester.runGraphQLTests("require-id-when-available", rule, {
+  valid: [
+    { ...WITH_SCHEMA, code: `query { noId { name } }` },
+    { ...WITH_SCHEMA, code: `query { hasId { id name } }` },
+    { ...WITH_SCHEMA, code: `query { hasId { ...HasIdFields } }` },
+    {
+      ...WITH_SCHEMA,
+      code: `query { vehicles { id ...on Car { id mileage } } }`,
+    },
+    { ...WITH_SCHEMA, code: `query { vehicles { ...on Car { id mileage } } }` },
+    { ...WITH_SCHEMA, code: `query { flying { ...on Bird { id } } }` },
+    {
+      ...WITH_SCHEMA,
+      code: `query { hasId { name } }`,
+      options: [{ fieldName: "name" }],
+    },
+    {
+      ...WITH_SCHEMA,
+      code: `query { vehicles { id ...on Car { mileage } } }`,
+    },
+  ],
+  invalid: [
+    {
+      ...WITH_SCHEMA,
+      code: `query { hasId { name } }`,
+      errors: [{ messageId: "REQUIRE_ID_WHEN_AVAILABLE" }],
+    },
+    {
+      ...WITH_SCHEMA,
+      code: `query { hasId { id } }`,
+      options: [{ fieldName: "name" }],
+      errors: [{ messageId: "REQUIRE_ID_WHEN_AVAILABLE" }],
+    },
+  ],
+});
---
+++ packages/graphql-eslint-rules/src/require-id-when-available.ts
@@ -0,0 +1,188 @@
+/*
+ * Taken from https://github.com/dotansimha/graphql-eslint/blob/300f73be802bdd06432a5df34939521d1ce0d93b/packages/plugin/src/rules/require-id-when-available.ts
+ * MIT license https://github.com/dotansimha/graphql-eslint/blob/300f73be802bdd06432a5df34939521d1ce0d93b/LICENSE
+ */
+
+import {
+  requireGraphQLSchemaFromContext,
+  requireSiblingsOperations,
+} from "../utils";
+import { GraphQLESLintRule } from "../types";
+import { GraphQLInterfaceType, GraphQLObjectType } from "graphql";
+import { getBaseType } from "../estree-parser";
+
+const REQUIRE_ID_WHEN_AVAILABLE = "REQUIRE_ID_WHEN_AVAILABLE";
+const DEFAULT_ID_FIELD_NAME = "id";
+
+type RequireIdWhenAvailableRuleConfig = { fieldName: string };
+
+const rule: GraphQLESLintRule<[RequireIdWhenAvailableRuleConfig], true> = {
+  meta: {
+    type: "suggestion",
+    docs: {
+      category: "Operations",
+      description:
+        "Enforce selecting specific fields when they are available on the GraphQL type.",
+      url:
+        "https://github.com/dotansimha/graphql-eslint/blob/master/docs/rules/require-id-when-available.md",
+      requiresSchema: true,
+      requiresSiblings: true,
+      examples: [
+        {
+          title: "Incorrect",
+          code: /* GraphQL */ `
+            # In your schema
+            type User {
+              id: ID!
+              name: String!
+            }
+            # Query
+            query user {
+              user {
+                name
+              }
+            }
+          `,
+        },
+        {
+          title: "Correct",
+          code: /* GraphQL */ `
+            # In your schema
+            type User {
+              id: ID!
+              name: String!
+            }
+            # Query
+            query user {
+              user {
+                id
+                name
+              }
+            }
+          `,
+        },
+      ],
+      recommended: true,
+    },
+    messages: {
+      [REQUIRE_ID_WHEN_AVAILABLE]: `Field "{{ fieldName }}" must be selected when it's available on a type. Please make sure to include it in your selection set!\nIf you are using fragments, make sure that all used fragments {{ checkedFragments }} specifies the field "{{ fieldName }}".`,
+    },
+    schema: {
+      type: "array",
+      maxItems: 1,
+      items: {
+        type: "object",
+        additionalProperties: false,
+        properties: {
+          fieldName: {
+            type: "string",
+            default: DEFAULT_ID_FIELD_NAME,
+          },
+        },
+      },
+    },
+  },
+  create(context) {
+    return {
+      SelectionSet(node) {
+        requireGraphQLSchemaFromContext("require-id-when-available", context);
+        const siblings = requireSiblingsOperations(
+          "require-id-when-available",
+          context
+        );
+
+        const fieldName =
+          (context.options[0] || {}).fieldName || DEFAULT_ID_FIELD_NAME;
+
+        if (!node.selections || node.selections.length === 0) {
+          return;
+        }
+
+        const typeInfo = node.typeInfo();
+        if (typeInfo && typeInfo.gqlType) {
+          const rawType = getBaseType(typeInfo.gqlType);
+          if (
+            rawType instanceof GraphQLObjectType ||
+            rawType instanceof GraphQLInterfaceType
+          ) {
+            const fields = rawType.getFields();
+            const hasIdFieldInType = !!fields[fieldName];
+            const checkedFragmentSpreads: Set<string> = new Set();
+
+            if (hasIdFieldInType) {
+              let found = false;
+
+              for (const selection of node.selections) {
+                if (
+                  selection.kind === "Field" &&
+                  selection.name.value === fieldName
+                ) {
+                  found = true;
+                } else if (selection.kind === "InlineFragment") {
+                  found = (selection.selectionSet?.selections || []).some(
+                    (s) => s.kind === "Field" && s.name.value === fieldName
+                  );
+                } else if (selection.kind === "FragmentSpread") {
+                  const foundSpread = siblings.getFragment(
+                    selection.name.value
+                  );
+
+                  if (foundSpread[0]) {
+                    checkedFragmentSpreads.add(
+                      foundSpread[0].document.name.value
+                    );
+
+                    found = (
+                      foundSpread[0].document.selectionSet?.selections || []
+                    ).some(
+                      (s) => s.kind === "Field" && s.name.value === fieldName
+                    );
+                  }
+                }
+
+                if (found) {
+                  break;
+                }
+              }
+
+              const { parent } = node as any;
+              const hasIdFieldInInterfaceSelectionSet =
+                parent &&
+                parent.kind === "InlineFragment" &&
+                parent.parent &&
+                parent.parent.kind === "SelectionSet" &&
+                parent.parent.selections.some(
+                  (s) => s.kind === "Field" && s.name.value === fieldName
+                );
+
+              if (!found && !hasIdFieldInInterfaceSelectionSet) {
+                context.report({
+                  loc: {
+                    start: {
+                      line: node.loc.start.line,
+                      column: node.loc.start.column - 1,
+                    },
+                    end: {
+                      line: node.loc.end.line,
+                      column: node.loc.end.column - 1,
+                    },
+                  },
+                  messageId: REQUIRE_ID_WHEN_AVAILABLE,
+                  data: {
+                    checkedFragments:
+                      checkedFragmentSpreads.size === 0
+                        ? ""
+                        : `(${Array.from(checkedFragmentSpreads).join(", ")})`,
+                    fieldName,
+                  },
+                });
+              }
+            }
+          }
+        }
+      },
+    };
+  },
+};
+
+export default rule;
