3319268009699af53ac4467bff1b2d85ce67aac2
Eloy Durán <eloy.de.enige@gmail.com>
2021-11-30 23:32:59 +0100

    [ARRDT] Test cov for fragments on Query type

---
+++ packages/apollo-react-relay-duct-tape/src/storeObservation/__generated__/compiledHooks_QueryTypeFragment.graphql.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+;
+
+import { FragmentRefs } from "@graphitation/apollo-react-relay-duct-tape";
+export type compiledHooks_QueryTypeFragment = {
+    readonly nonNode: {
+        readonly id: string | null;
+    };
+    readonly " $refType": "compiledHooks_QueryTypeFragment";
+};
+export type compiledHooks_QueryTypeFragment$data = compiledHooks_QueryTypeFragment;
+export type compiledHooks_QueryTypeFragment$key = {
+    readonly " $data"?: compiledHooks_QueryTypeFragment$data;
+    readonly " $fragmentRefs": FragmentRefs<"compiledHooks_QueryTypeFragment">;
+};
