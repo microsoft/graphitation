6db3cf9f6ab8dfeccc6555e3ed309792372bcf53
Eloy Durán <eloy.de.enige@gmail.com>
2021-12-13 16:51:43 +0100

    [compiler] Move fragment metadata extraction

--- packages/relay-compiler-language-graphitation/src/formatModuleTransforms/extractMetadataTransform.ts
+++ packages/relay-compiler-language-graphitation/src/formatModuleTransforms/extractMetadataTransform.ts
@@ -1,13 +1,27 @@
-import { DocumentNode, OperationDefinitionNode, visit } from "graphql";
+import {
+  ArgumentNode,
+  DocumentNode,
+  FieldNode,
+  FragmentDefinitionNode,
+  FragmentSpreadNode,
+  OperationDefinitionNode,
+  visit,
+} from "graphql";
 import invariant from "invariant";
 
 interface ConnectionMetadata {
-  countVariable: string;
-  cursorVariable: string;
+  forwardCountVariable?: string;
+  forwardCursorVariable?: string;
+  backwardCountVariable?: string;
+  backwardCursorVariable?: string;
   selectionPath: string[];
 }
 export interface Metadata {
   rootSelection?: string;
+  mainFragment?: {
+    name: string;
+    typeCondition: string;
+  };
   connection?: ConnectionMetadata;
 }
 
@@ -19,18 +33,72 @@ export function extractMetadataTransform(
   document: DocumentNode
 ): Metadata | undefined {
   const metadata: Metadata = {};
-
   const nodeFieldSelection = extractNodeFieldSelection(document);
   if (nodeFieldSelection) {
     metadata.rootSelection = "node";
   }
-
+  const mainFragment = getMainFragmentMetadata(
+    document,
+    nodeFieldSelection?.name.value
+  );
+  if (mainFragment) {
+    metadata.mainFragment = mainFragment;
+  }
   const connectionMetadata = extractConnectionMetadataTransform(document);
   if (connectionMetadata) {
     metadata.connection = connectionMetadata;
   }
-
   return Object.keys(metadata).length > 0 ? metadata : undefined;
+}
+
+function getMainFragmentMetadata(
+  document: DocumentNode,
+  rootSelection: string | undefined
+):
+  | {
+      name: string;
+      typeCondition: string;
+    }
+  | undefined {
+  const [
+    operationDefinition,
+    ...fragmentDefinitions
+  ] = document.definitions as [
+    OperationDefinitionNode,
+    ...FragmentDefinitionNode[]
+  ];
+  invariant(
+    operationDefinition.kind === "OperationDefinition" &&
+      fragmentDefinitions.every((node) => node.kind === "FragmentDefinition"),
+    "Expected definition nodes in specific order"
+  );
+  if (fragmentDefinitions.length === 0) {
+    return undefined;
+  }
+  let selectionSet = operationDefinition.selectionSet;
+  if (rootSelection) {
+    const field = selectionSet.selections.find(
+      (selection) =>
+        selection.kind === "Field" && selection.name.value === rootSelection
+    ) as FieldNode | undefined;
+    invariant(
+      field?.selectionSet,
+      "Expected root selection to exist in document"
+    );
+    selectionSet = field.selectionSet;
+  }
+  const mainFragmentSpread = selectionSet.selections.find(
+    (selection) => selection.kind === "FragmentSpread"
+  ) as FragmentSpreadNode | undefined;
+  invariant(mainFragmentSpread, "Expected a main fragment spread");
+  const mainFragment = fragmentDefinitions.find(
+    (fragment) => fragment.name.value === mainFragmentSpread.name.value
+  );
+  invariant(mainFragment, "Expected a main fragment");
+  return {
+    name: mainFragment.name.value,
+    typeCondition: mainFragment.typeCondition.name.value,
+  };
 }
 
 function extractNodeFieldSelection(document: DocumentNode) {
@@ -40,7 +108,7 @@ function extractNodeFieldSelection(document: DocumentNode) {
   invariant(operationDefinition, "Expected an operation");
   const nodeFieldSelection = operationDefinition.selectionSet.selections.find(
     (selection) => selection.kind === "Field" && selection.name.value === "node"
-  );
+  ) as FieldNode | undefined;
   return nodeFieldSelection;
 }
 
@@ -48,57 +116,65 @@ function extractConnectionMetadataTransform(
   document: DocumentNode
 ): ConnectionMetadata | undefined {
   let foundConnection = false;
-  let countVariable: string | undefined;
-  let cursorVariable: string | undefined;
-  const selectionPath: string[] = [];
+  const metadata: ConnectionMetadata = { selectionPath: [] };
   visit(document, {
     Field: {
       enter(fieldNode) {
         if (!foundConnection) {
-          selectionPath.push(fieldNode.name.value);
+          metadata.selectionPath.push(fieldNode.name.value);
         }
         if (
           fieldNode.directives?.find(
             (directive) => directive.name.value === "connection"
           )
         ) {
-          invariant(!foundConnection, "Expected to find a single connection");
+          invariant(
+            !foundConnection,
+            "Expected to find a single connection in one document"
+          );
           foundConnection = true;
 
-          const countArgument = fieldNode.arguments?.find(
-            (arg) => arg.name.value === "first"
-          );
-          countVariable =
-            countArgument?.value.kind === "Variable"
-              ? countArgument.value.name.value
-              : undefined;
-          const cursorArgument = fieldNode.arguments?.find(
-            (arg) => arg.name.value === "after"
-          );
-          cursorVariable =
-            cursorArgument?.value.kind === "Variable"
-              ? cursorArgument.value.name.value
-              : undefined;
+          fieldNode.arguments?.forEach((arg) => {
+            switch (arg.name.value) {
+              case "first": {
+                metadata.forwardCountVariable = getVariableValue(arg);
+                break;
+              }
+              case "after": {
+                metadata.forwardCursorVariable = getVariableValue(arg);
+                break;
+              }
+              case "last": {
+                metadata.backwardCountVariable = getVariableValue(arg);
+                break;
+              }
+              case "before": {
+                metadata.backwardCursorVariable = getVariableValue(arg);
+                break;
+              }
+            }
+          });
         }
       },
       leave() {
         if (!foundConnection) {
-          selectionPath.pop();
+          metadata.selectionPath.pop();
         }
       },
     },
   });
   if (foundConnection) {
     invariant(
-      countVariable,
-      "Expected connection to have a variable count argument"
+      (metadata.forwardCountVariable && metadata.forwardCursorVariable) ||
+        (metadata.backwardCountVariable && metadata.backwardCursorVariable),
+      "Expected correct count and cursor variables combinations"
     );
-    invariant(
-      cursorVariable,
-      "Expected connection to have a variable cursor argument"
-    );
-    return { countVariable, cursorVariable, selectionPath };
+    return metadata;
   } else {
     return undefined;
   }
+}
+
+function getVariableValue(arg: ArgumentNode) {
+  return arg.value.kind === "Variable" ? arg.value.name.value : undefined;
 }
