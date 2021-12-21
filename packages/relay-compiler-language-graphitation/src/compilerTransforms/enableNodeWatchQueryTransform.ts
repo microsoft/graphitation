57689edb7871903a55733098cb36c37c6364d4ca
Eloy Durán <eloy.de.enige@gmail.com>
2021-12-21 23:10:20 +0100

    [ARRDT] Add some descriptions to the various transforms

--- packages/relay-compiler-language-graphitation/src/compilerTransforms/enableNodeWatchQueryTransform.ts
+++ packages/relay-compiler-language-graphitation/src/compilerTransforms/enableNodeWatchQueryTransform.ts
@@ -2,6 +2,12 @@ import { Directive, Fragment } from "relay-compiler";
 import { IRTransform } from "relay-compiler/lib/core/CompilerContext";
 import { implementsNodeInterface } from "./utils";
 
+/**
+ * Uses relay-compiler's `@refetchable` directive to emit a query operation for
+ * a single fragment (on the `Node` interface or `Query` type). This operation
+ * is used by the `use*Fragment` hooks to observe the Apollo Client store for
+ * changes to _just_ that data selected in the fragment.
+ */
 export const enableNodeWatchQueryTransform: IRTransform = (context) => {
   let nextContext = context;
 
