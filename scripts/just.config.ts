1f3e620e2de631a6a27a8cac1ac4b4a0b396938a
Eloy Durán <eloy.de.enige@gmail.com>
2021-12-22 23:52:50 +0100

    [monorepo] Emit sourcemaps

--- scripts/just.config.ts
+++ scripts/just.config.ts
@@ -22,6 +22,7 @@ export const build = () => {
     entryPoints: glob.sync(["src/**/*.{ts,tsx}", "!src/**/__tests__/**"]),
     outdir: "lib",
     target: "es6",
+    sourcemap: "external",
   };
   return parallel(
     esbuildTask({
