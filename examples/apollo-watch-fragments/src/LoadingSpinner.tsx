bba763b9032479c2a2e76c68816817b89556fe29
Eloy Durán <eloy.de.enige@gmail.com>
2021-12-22 18:43:37 +0100

    [example] Show loading spinner while paginating

---
+++ examples/apollo-watch-fragments/src/LoadingSpinner.tsx
@@ -0,0 +1,23 @@
+/**
+ * https://loading.io/css/
+ * https://github.com/loadingio/css-spinner/tree/master/src/spinner
+ */
+
+import React from "react";
+
+export const LoadingSpinner: React.FC = () => (
+  <div className="lds-spinner">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
