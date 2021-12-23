5b550fbec9fc3b0cd1c53601055953a39dd92ea1
Eloy Durán <eloy.de.enige@gmail.com>
2021-12-23 22:11:09 +0100

    [example] Make use of uncompiled types

--- packages/relay-compiler-language-graphitation/src/types.ts
+++ packages/relay-compiler-language-graphitation/src/types.ts
@@ -2,6 +2,8 @@ import { DocumentNode } from "graphql";
 
 import { Metadata } from "./formatModuleTransforms/extractMetadataTransform";
 
+export { Metadata };
+
 export interface CompiledArtefactModule {
   executionQueryDocument?: DocumentNode;
   watchQueryDocument?: DocumentNode;
