8db042c333bca7e7acce5602fd1be07f220e749b
Eloy Durán <eloy.de.enige@gmail.com>
2022-01-04 19:42:08 +0100

    [ARRDT] Re-export types separately to avoid bundler confusion

--- packages/apollo-react-relay-duct-tape/src/storeObservation/compiledHooks/index.ts
+++ packages/apollo-react-relay-duct-tape/src/storeObservation/compiledHooks/index.ts
@@ -1,11 +1,7 @@
 export { useCompiledLazyLoadQuery } from "./useCompiledLazyLoadQuery";
 export { useCompiledFragment } from "./useCompiledFragment";
-export {
-  useCompiledRefetchableFragment,
-  RefetchFn,
-  Disposable,
-} from "./useCompiledRefetchableFragment";
-export {
-  useCompiledPaginationFragment,
-  PaginationFn,
-} from "./useCompiledPaginationFragment";
+export { useCompiledRefetchableFragment } from "./useCompiledRefetchableFragment";
+export { useCompiledPaginationFragment } from "./useCompiledPaginationFragment";
+
+export type { RefetchFn, Disposable } from "./useCompiledRefetchableFragment";
+export type { PaginationFn } from "./useCompiledPaginationFragment";
