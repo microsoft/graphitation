f2ef46d9f17811e2a9a2b01f813addd1662a3f74
Eloy Durán <eloy.de.enige@gmail.com>
2022-01-05 15:11:41 +0100

    Changes for testing with React v18

--- examples/apollo-watch-fragments/src/index.tsx
+++ examples/apollo-watch-fragments/src/index.tsx
@@ -8,7 +8,9 @@ import { createClient } from "./graphql";
 
 const client = createClient();
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
