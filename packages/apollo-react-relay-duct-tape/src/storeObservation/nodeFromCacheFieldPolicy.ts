441a07a03596f805863b3014b23d1cc2056da9fd
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2022-02-09 22:01:12 +0100

    Memoize fragment document for apollo readFragment (#117)
    * Memoize fragment document for apollo readFragment
    
    This will help us re-use the same optimism cache key (vs creating a new cache entry on each readFragment call)
    
    * Update packages/apollo-react-relay-duct-tape/src/storeObservation/nodeFromCacheFieldPolicy.ts
    
    Co-authored-by: Eloy Durán <eloy.de.enige@gmail.com>
    
    * Update packages/apollo-react-relay-duct-tape/src/storeObservation/nodeFromCacheFieldPolicy.ts
    
    Co-authored-by: Eloy Durán <eloy.de.enige@gmail.com>
    
    * Update packages/apollo-react-relay-duct-tape/src/storeObservation/nodeFromCacheFieldPolicy.ts
    
    Co-authored-by: Eloy Durán <eloy.de.enige@gmail.com>
    
    Co-authored-by: Eloy Durán <eloy.de.enige@gmail.com>

--- packages/apollo-react-relay-duct-tape/src/storeObservation/nodeFromCacheFieldPolicy.ts
+++ packages/apollo-react-relay-duct-tape/src/storeObservation/nodeFromCacheFieldPolicy.ts
@@ -2,6 +2,8 @@ import { FieldReadFunction } from "@apollo/client";
 import { FragmentSpreadNode, FragmentDefinitionNode } from "graphql";
 import invariant from "invariant";
 
+const FRAGMENT_DOCUMENT_CACHE = new WeakMap();
+
 /**
  * Use this as the field policy function for the node root field, which is what
  * gets invoked when using relay-compiler's refetch query. Queries that use a
@@ -38,17 +40,24 @@ export const nodeFromCacheFieldPolicy: FieldReadFunction = (
     fragmentName
   );
 
+  let fragmentDocument = FRAGMENT_DOCUMENT_CACHE.get(options.query);
+
+  if (!fragmentDocument) {
+    fragmentDocument = {
+      kind: "Document",
+      definitions: options.query.definitions.filter(
+        (def) => def.kind === "FragmentDefinition"
+      ),
+    };
+    FRAGMENT_DOCUMENT_CACHE.set(options.query, fragmentDocument);
+  }
+
   const id = `${fragment.typeCondition.name.value}:${nodeId}`;
   const data = options.cache.readFragment({
     id,
     variables: options.variables,
     fragmentName,
-    fragment: {
-      kind: "Document",
-      definitions: options.query.definitions.filter(
-        (def) => def.kind === "FragmentDefinition"
-      ),
-    },
+    fragment: fragmentDocument,
   });
   invariant(data, "Expected to find cached data with id `%s`", id);
 
