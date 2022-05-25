4087fd885b0ac95afa00bc9f7c29b269fe5cae17
Eloy Durán <eloy.de.enige@gmail.com>
2022-05-25 22:57:45 +0200

    [duct-tape] Make artefact import transform work for es5 modules

--- packages/apollo-react-relay-duct-tape/src/storeObservation/createImportDocumentsTransform.test.ts
+++ packages/apollo-react-relay-duct-tape/src/storeObservation/createImportDocumentsTransform.test.ts
@@ -1,6 +1,11 @@
 import * as ts from "typescript";
 import { createImportDocumentsTransform } from "./createImportDocumentsTransform";
 
+import * as _fs from "fs";
+const fs = _fs as jest.Mocked<typeof _fs>;
+
+jest.mock("fs");
+
 expect.addSnapshotSerializer({
   test: (val) => typeof val === "string",
   print: (val) => {
@@ -8,45 +13,67 @@ expect.addSnapshotSerializer({
   },
 });
 
-function transform(sourceText: string): string {
-  const sourceFile = ts.createSourceFile(
-    "SomeComponent.tsx",
-    sourceText,
-    ts.ScriptTarget.Latest
-  );
-  const result = ts.transform(sourceFile, [createImportDocumentsTransform()]);
-  const printer = ts.createPrinter();
-  return printer.printFile(result.transformed[0]);
-}
-
 describe(createImportDocumentsTransform, () => {
-  it("works with documents without interpolation", () => {
-    const source = `
+  describe.each([
+    [
+      "es6",
+      {
+        target: ts.ScriptTarget.ES2015,
+        module: ts.ModuleKind.ES2015,
+      },
+    ],
+    [
+      "es5",
+      {
+        target: ts.ScriptTarget.ES5,
+        module: ts.ModuleKind.CommonJS,
+      },
+    ],
+  ])("when emitting %s modules", (_targetName, compilerOptions) => {
+    function transform(sourceText: string): string {
+      return ts.transpileModule(sourceText, {
+        fileName: __filename,
+        transformers: { before: [createImportDocumentsTransform()] },
+        compilerOptions,
+      }).outputText;
+    }
+
+    it("works with documents without interpolation", () => {
+      fs.existsSync.mockImplementation(() => true);
+      const source = `
+        const doc = graphql\`
+          fragment SomeComponentFragment on Query {
+            helloWorld
+          }
+        \`
+      `;
+      expect(transform(source)).toMatchSnapshot();
+    });
+
+    it("works with documents with interpolation", () => {
+      fs.existsSync.mockImplementation(() => true);
+      const source = `
+        const doc = graphql\`
+          query SomeComponentQuery($id: ID!) {
+            helloWorld
+            ...SomeOtherComponentFragment
+          }
+          \${SomeOtherComponentFragment}
+        \`
+      `;
+      expect(transform(source)).toMatchSnapshot();
+    });
+
+    it("does not emit an import for a fragment on non-Node/Query types (for which no artefacts are emitted)", () => {
+      fs.existsSync.mockImplementation(() => false);
+      const source = `
       const doc = graphql\`
         fragment SomeComponentFragment on Query {
           helloWorld
         }
       \`
     `;
-    expect(transform(source)).toMatchInlineSnapshot(`
-      import { documents as __graphitation_generatedQueries_SomeComponentFragment } from "./__generated__/SomeComponentWatchNodeQuery.graphql";
-      const doc = __graphitation_generatedQueries_SomeComponentFragment;
-    `);
-  });
-
-  it("works with documents with interpolation", () => {
-    const source = `
-      const doc = graphql\`
-        query SomeComponentQuery($id: ID!) {
-          helloWorld
-          ...SomeOtherComponentFragment
-        }
-        \${SomeOtherComponentFragment}
-      \`
-    `;
-    expect(transform(source)).toMatchInlineSnapshot(`
-      import { documents as __graphitation_generatedQueries_SomeComponentQuery } from "./__generated__/SomeComponentQuery.graphql";
-      const doc = __graphitation_generatedQueries_SomeComponentQuery;
-    `);
+      expect(transform(source)).toMatchSnapshot();
+    });
   });
 });
