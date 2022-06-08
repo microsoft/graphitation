231ef6b07ce9177437499a0933415a3bbabf08fa
Jakub Vejr <jakubvejr@microsoft.com>
2022-06-08 14:32:54 +0200

    GraphQL codegen plugins - Supermassive typed document node

---
+++ packages/graphql-codegen-plugins/src/index.ts
@@ -0,0 +1,9 @@
+import {
+  plugin as supermassiveTypedDocumentPlugin,
+  validate as supermassiveTypedDocumentValidate,
+} from "./supermassive-typed-document-node";
+
+export const SupermassiveTypedDocument = {
+  plugin: supermassiveTypedDocumentPlugin,
+  validate: supermassiveTypedDocumentValidate,
+};
