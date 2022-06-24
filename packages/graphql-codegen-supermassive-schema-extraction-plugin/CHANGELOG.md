bd323a80de24836b7b70e271292d3b345bc03830
Jakub Vejr <jakubvejr@microsoft.com>
2022-06-24 16:13:02 +0200

    [SUPERMASSIVE_SCHEMA_EXTRACTION_PLUGIN] supermassive schema extraction plugin added

---
+++ packages/graphql-codegen-supermassive-schema-extraction-plugin/CHANGELOG.md
@@ -0,0 +1,30 @@
+# Change Log - @graphitation/graphql-codegen-supermassive-typed-document-node-plugin
+
+This log was last generated on Mon, 20 Jun 2022 12:54:16 GMT and should not be manually modified.
+
+<!-- Start content -->
+
+## 0.1.2
+
+Mon, 20 Jun 2022 12:54:16 GMT
+
+### Patches
+
+- version bump - npmignore modified (jakubvejr@microsoft.com)
+
+## 0.1.1
+
+Thu, 16 Jun 2022 21:57:11 GMT
+
+### Patches
+
+- No-op bump (eloy.de.enige@gmail.com)
+
+## 0.1.0
+
+Fri, 10 Jun 2022 14:09:50 GMT
+
+### Minor changes
+
+- Supermassive typed document node plugin added (jakubvejr@microsoft.com)
+- Bump @graphitation/supermassive to v0.8.3
---
+++ packages/graphql-codegen-supermassive-schema-extraction-plugin/src/index.ts
@@ -0,0 +1,38 @@
+import {
+  Types,
+  PluginValidateFn,
+  PluginFunction,
+} from "@graphql-codegen/plugin-helpers";
+import ts from "typescript";
+import { GraphQLSchema } from "graphql";
+import { printSchema, parse } from "graphql";
+import { extractImplicitTypesToTypescript } from "@graphitation/supermassive";
+import { extname } from "path";
+import { RawClientSideBasePluginConfig } from "@graphql-codegen/visitor-plugin-common";
+
+type PluginConfig = RawClientSideBasePluginConfig & {
+  files: string[];
+};
+
+export const plugin: PluginFunction<PluginConfig> = (schema: GraphQLSchema) => {
+  const tsContents: ts.SourceFile = extractImplicitTypesToTypescript(
+    parse(printSchema(schema)),
+  );
+
+  const printer = ts.createPrinter();
+
+  return printer.printNode(ts.EmitHint.SourceFile, tsContents, tsContents);
+};
+
+export const validate: PluginValidateFn<PluginConfig> = async (
+  schema: GraphQLSchema,
+  documents: Types.DocumentFile[],
+  config,
+  outputFile: string,
+) => {
+  if (extname(outputFile) !== ".ts" && extname(outputFile) !== ".tsx") {
+    throw new Error(
+      `Plugin "supermassive-typed-document-node" requires extension to be ".ts" or ".tsx"!`,
+    );
+  }
+};
