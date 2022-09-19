fa42afe61f55c3c4f9cbe388c40456e9cf8ea578
Mikhail Novikov <mnovikov@microsoft.com>
2022-09-19 17:10:46 +0200

    Outputting IR

--- packages/relay-compiler-language-graphitation/src/formatModule.ts
+++ packages/relay-compiler-language-graphitation/src/formatModule.ts
@@ -4,10 +4,31 @@
 /* istanbul ignore file */
 
 import { FormatModule } from "relay-compiler/lib/language/RelayLanguagePluginInterface";
+import { parse } from "graphql";
 
-export const formatModule: FormatModule = ({ hash, typeText }) =>
-  `/* tslint:disable */
+export const formatModule: FormatModule = ({
+  hash,
+  typeText,
+  concreteText,
+  node,
+  documentType,
+  docText,
+  definition,
+  kind,
+}) => {
+  console.log(documentType, kind, docText, definition);
+  return `/* tslint:disable */
 /* eslint-disable */
 // @ts-nocheck
 ${hash ? `/* ${hash} */\n` : ""};
-${typeText || ""}`;
+${typeText || ""}
+
+export const node = ${concreteText};
+
+export const document = ${JSON.stringify(
+    parse(definition.loc.source.body),
+    null,
+    2,
+  )};
+`;
+};
