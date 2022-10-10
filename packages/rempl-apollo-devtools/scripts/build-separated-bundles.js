f13754c2ed38db371115a0af04c0f6b0f6067cb6
vejrj <77059398+vejrj@users.noreply.github.com>
2022-10-10 18:58:34 +0200

    Dependencies update  (#223)
    * [REMPL_APOLLO_DEVTOOLS] Dependencies update - graphiql

--- packages/rempl-apollo-devtools/scripts/build-separated-bundles.js
+++ packages/rempl-apollo-devtools/scripts/build-separated-bundles.js
@@ -4,12 +4,22 @@ var { resolve } = require("path");
 
 if (require.main === module) {
   (async () => {
-    const __APOLLO_DEVTOOLS_SUBSCRIBER__ = (
-      await esbuild.build({
+    return [
+      esbuild.buildSync({
+        entryPoints: ["src/publisher/index.ts"],
+        write: true,
+        bundle: true,
+        minify: true,
+        sourcemap: false,
+        outfile: "dist/apollo-devtools-publisher.js",
+        format: "iife",
+      }),
+      esbuild.buildSync({
         entryPoints: ["src/subscriber/index.tsx"],
-        write: false,
+        write: true,
         minify: false,
         bundle: true,
+        outfile: "dist/apollo-devtools-subscriber.js",
         format: "esm",
         sourcemap: false,
         define: {
@@ -20,25 +30,13 @@ if (require.main === module) {
             ),
           ),
           __GLOBAL_CSS__: JSON.stringify(
-            readFileSync(resolve(process.cwd(), "scripts/styles.css"), "utf8"),
+            readFileSync(
+              resolve(process.cwd(), "src/subscriber/styles.css"),
+              "utf8",
+            ),
           ),
         },
-      })
-    ).outputFiles[0].text;
-
-    return esbuild.buildSync({
-      entryPoints: ["src/publisher/index.ts"],
-      write: true,
-      bundle: true,
-      minify: true,
-      sourcemap: false,
-      outfile: "dist/apollo-devtools.js",
-      format: "iife",
-      define: {
-        __APOLLO_DEVTOOLS_SUBSCRIBER__: JSON.stringify(
-          __APOLLO_DEVTOOLS_SUBSCRIBER__,
-        ),
-      },
-    });
+      }),
+    ];
   })();
 }
