433a5f544a6ec7c35f0ca0167b5f70885e45f597
Jakub Vejr <jakubvejr@microsoft.com>
2022-11-01 14:56:24 +0100

    fix

--- packages/rempl-apollo-devtools/src/publisher/rempl-wrapper.ts
+++ packages/rempl-apollo-devtools/src/publisher/rempl-wrapper.ts
@@ -21,14 +21,10 @@ export class RemplWrapper {
 
   constructor(enableRemplHotkey: string) {
     this.publisher = createPublisher("apollo-devtools", () => {
-      if (__APOLLO_DEVTOOLS_SUBSCRIBER__) {
-        return { type: "script", value: __APOLLO_DEVTOOLS_SUBSCRIBER__ };
-      } else {
-        return {
-          type: "url",
-          value: window.__REMPL_APOLLO_DEVTOOLS_URL__ || "",
-        };
-      }
+      return {
+        type: "url",
+        value: window.__REMPL_APOLLO_DEVTOOLS_URL__ || "",
+      };
     });
 
     this.attachMethodsToPublisher(this.publisher);
