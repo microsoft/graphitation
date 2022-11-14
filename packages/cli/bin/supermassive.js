fa149ef1ae04a59c5549886dd6146e3678cdf953
Jakub Vejr <jakubvejr@microsoft.com>
2022-11-14 11:23:12 +0100

    codegen and cli renamed

--- packages/cli/bin/supermassive.js
+++ packages/cli/bin/supermassive.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 (function () {
-  require("@graphitation/graphitation-cli/lib/supermassive")
+  require("@graphitation/cli/lib/supermassive")
     .supermassive()
     .parseAsync(process.argv);
 })();
