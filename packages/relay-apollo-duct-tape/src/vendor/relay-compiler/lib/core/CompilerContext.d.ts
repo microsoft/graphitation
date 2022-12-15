107451da9a9b97b002b285e02303e06d4a665648
Eloy Durán <eloy.de.enige@gmail.com>
2022-12-15 23:22:08 +0100

    Fix type errors

--- packages/relay-apollo-duct-tape/src/vendor/relay-compiler/lib/core/CompilerContext.d.ts
+++ packages/relay-apollo-duct-tape/src/vendor/relay-compiler/lib/core/CompilerContext.d.ts
@@ -1,4 +1,4 @@
 // This export in DT is wrong, but it's an old version and relay-compiler
 // no longer exists as JS, so whatever.
-import { CompilerContext } from "relay-compiler/lib/core/CompilerContext";
+import { CompilerContext } from "relay-compiler";
 export default CompilerContext;
