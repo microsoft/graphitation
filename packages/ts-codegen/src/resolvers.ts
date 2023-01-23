1ddd57cde7ba31b4703427e257db0ee5e8324eee
Mikhail Novikov <mnovikov@microsoft.com>
2023-01-23 18:58:30 +0100

    WIP

--- packages/ts-codegen/src/resolvers.ts
+++ packages/ts-codegen/src/resolvers.ts
@@ -7,366 +7,153 @@ import {
   Kind,
 } from "graphql";
 import { ASTReducer, visit } from "./typedVisitor";
-import { TsCodegenContext } from "./context";
+import {
+  Field,
+  InputObjectType,
+  ObjectType,
+  TsCodegenContext,
+  Type,
+} from "./context";
 import {
   createNullableType,
   createNonNullableType,
   getResolverReturnType,
   getAncestorEntity,
+  createTypeFromNode,
 } from "./utilities";
 
 export function generateResolvers(
   context: TsCodegenContext,
   document: DocumentNode,
 ): ts.SourceFile {
-  return visit(document, createResolversReducer(context)) as ts.SourceFile;
+  const statements = context
+    .getAllTypes()
+    .map((type) => createResolversForType(context, type))
+    .filter((t) => t != null) as ts.Statement[];
+  const imports = context.getAllResolverImportDeclarations() as ts.Statement[];
+  return factory.createSourceFile(
+    imports.concat(statements),
+    factory.createToken(ts.SyntaxKind.EndOfFileToken),
+    ts.NodeFlags.None,
+  );
 }
 
-type ASTReducerMap = {
-  Document: ts.SourceFile;
-  SchemaExtension: null;
-  ObjectTypeDefinition: ts.ModuleDeclaration;
-  ObjectTypeExtension: ts.ModuleDeclaration;
-  FieldDefinition: ts.TypeAliasDeclaration;
-  NameNode: string;
-
-  NamedType: ts.TypeNode;
-  ListType: ts.TypeNode;
-  NonNullType: ts.TypeNode;
-
-  Directive: null;
-
-  InputFieldDefinition: ts.TypeElement;
-  InputValueDefinition: ts.PropertySignature;
-  InputObjectTypeDefinition: ts.TypeAliasDeclaration;
-
-  UnionTypeDefinitionNode: null;
-  InterfaceTypeDefinitionNode: null;
-  EnumTypeDefinitionNode: null;
-  ScalarTypeDefinition: null;
-  DirectiveDefinition: null;
-};
-
-type ASTReducerFieldMap = {
-  Document: {
-    definitions: ts.Statement | ts.Statement[];
-  };
-  ObjectTypeDefinition: {
-    name: ASTReducerMap["NameNode"];
-    fields: ASTReducerMap["FieldDefinition"];
-  };
-  ObjectTypeExtension: {
-    name: ASTReducerMap["NameNode"];
-    fields: ASTReducerMap["FieldDefinition"];
-  };
-  FieldDefinition: {
-    name: ASTReducerMap["NameNode"];
-    type: ts.TypeNode;
-    arguments: ASTReducerMap["InputFieldDefinition"];
-  };
-  InputObjectTypeDefinition: {
-    name: ASTReducerMap["NameNode"];
-    fields: ASTReducerMap["FieldDefinition"];
-  };
-
-  NamedType: {
-    name: ASTReducerMap["NameNode"];
-  };
-  ListType: {
-    type: ts.TypeNode;
-  };
-  NonNullType: {
-    type: ts.TypeNode;
-  };
-
-  InputValueDefinition: {
-    name: ASTReducerMap["NameNode"];
-    type: ts.TypeNode;
-  };
-};
-
-function createResolversReducer(
+function createResolversForType(
   context: TsCodegenContext,
-): ASTReducer<ts.Node | string, ASTReducerMap, ASTReducerFieldMap> {
-  context.clearEntitiesToImport();
-  return {
-    Document: {
-      leave(node) {
-        const imports = context.getAllResolverImportDeclarations() as ts.Statement[];
-        const statements = node.definitions;
-        return factory.createSourceFile(
-          imports.concat(statements.flat()),
-          factory.createToken(ts.SyntaxKind.EndOfFileToken),
-          ts.NodeFlags.None,
-        );
-      },
+  type: Type,
+): ts.Statement | null {
+  switch (type.kind) {
+    case "OBJECT": {
+      return createObjectTypeResolvers(context, type);
+    }
+    case "INPUT_OBJECT": {
+      return createInputObjectType(context, type);
+    }
+    default: {
+      return null;
+    }
+  }
+}
+
+function createObjectTypeResolvers(
+  context: TsCodegenContext,
+  type: ObjectType,
+): ts.ModuleDeclaration | null {
+  const fields = type.fields.map((field) =>
+    createResolverField(context, type, field),
+  );
+  return factory.createModuleDeclaration(
+    undefined,
+    [
+      factory.createModifier(ts.SyntaxKind.ExportKeyword),
+      factory.createModifier(ts.SyntaxKind.DeclareKeyword),
+    ],
+    factory.createIdentifier(type.name),
+    factory.createModuleBlock(fields),
+    ts.NodeFlags.Namespace,
+  );
+}
+
+function createResolverField(
+  context: TsCodegenContext,
+  type: Type,
+  field: Field,
+): ts.TypeAliasDeclaration {
+  let modelIdentifier;
+  if (["Query", "Mutation", "Subscription"].includes(type.name)) {
+    modelIdentifier = factory.createKeywordTypeNode(
+      ts.SyntaxKind.UnknownKeyword,
+    );
+  } else {
+    context.addEntityToImport(type.name);
+    modelIdentifier = context.getModelType(type.name).toTypeReference();
+  }
+
+  const resolverParametersDefinitions = {
+    parent: {
+      name: "model",
+      type: modelIdentifier as ts.TypeReferenceNode,
     },
-    SchemaExtension: {
-      leave(): null {
-        return null;
-      },
-    },
-    ObjectTypeDefinition: {
-      leave(node): ts.ModuleDeclaration {
-        return factory.createModuleDeclaration(
+    args: {
+      name: "args",
+      type: field.arguments.map(({ name, type }) =>
+        factory.createPropertySignature(
+          [factory.createModifier(ts.SyntaxKind.ReadonlyKeyword)],
+          factory.createIdentifier(name),
           undefined,
-          [
-            factory.createModifier(ts.SyntaxKind.ExportKeyword),
-            factory.createModifier(ts.SyntaxKind.DeclareKeyword),
-          ],
-          factory.createIdentifier(node.name),
-          factory.createModuleBlock(node.fields || []),
-          ts.NodeFlags.Namespace,
-        );
-      },
+          createTypeFromNode(context, type),
+        ),
+      ),
     },
-    ObjectTypeExtension: {
-      leave(node): ts.ModuleDeclaration {
-        return factory.createModuleDeclaration(
-          undefined,
-          [
-            factory.createModifier(ts.SyntaxKind.ExportKeyword),
-            factory.createModifier(ts.SyntaxKind.DeclareKeyword),
-          ],
-          factory.createIdentifier(node.name),
-          factory.createModuleBlock(node.fields || []),
-          ts.NodeFlags.Namespace,
-        );
-      },
+    context: {
+      name: "context",
+      type: context.getContextType().toTypeReference(),
     },
-    FieldDefinition: {
-      leave(node, _a, _p, _path, ancestors): ts.TypeAliasDeclaration {
-        const parentObject = ancestors[ancestors.length - 1];
-        if (
-          !(
-            parentObject &&
-            "kind" in parentObject &&
-            (isTypeDefinitionNode(parentObject) ||
-              isTypeExtensionNode(parentObject))
-          )
-        ) {
-          throw new Error("Invalid parent for field");
-        }
-        const parentName = parentObject.name.value;
-        let modelIdentifier;
-        if (["Query", "Mutation", "Subscription"].includes(parentName)) {
-          modelIdentifier = factory.createKeywordTypeNode(
-            ts.SyntaxKind.UnknownKeyword,
-          );
-        } else if (parentObject?.kind !== Kind.INTERFACE_TYPE_DEFINITION) {
-          context.addEntityToImport(parentName);
-          modelIdentifier = context.getModelType(parentName).toTypeReference();
-        } else {
-          if (
-            getAncestorEntity(ancestors, parseInt(_path[3] as string, 10))
-              ?.kind === "ObjectTypeDefinition"
-          ) {
-            context.addEntityToImport(parentName);
-          }
-        }
-        const resolverParametersDefinitions = {
-          parent: {
-            name: "model",
-            type: modelIdentifier as ts.TypeReferenceNode,
-          },
-          args: { name: "args", type: node.arguments || [] },
-          context: {
-            name: "context",
-            type: context.getContextType().toTypeReference(),
-          },
-          resolveInfo: {
-            name: "info",
-            type: context.getResolveInfoType().toTypeReference(),
-          },
-        };
-
-        return factory.createTypeAliasDeclaration(
-          undefined,
-          [factory.createModifier(ts.SyntaxKind.ExportKeyword)],
-          factory.createIdentifier(node.name),
-          parentName === "Subscription"
-            ? [
-                factory.createTypeParameterDeclaration(
-                  factory.createIdentifier("A"),
-                  undefined,
-                  factory.createKeywordTypeNode(ts.SyntaxKind.UnknownKeyword),
-                ),
-              ]
-            : undefined,
-          getResolverReturnType(
-            node.type,
-            parentName,
-            resolverParametersDefinitions,
-          ),
-        );
-      },
-    },
-
-    NamedType: {
-      leave(node, _a, _p, path, ancestors): ts.TypeNode {
-        const isImplementedInterface = path[2] === "interfaces";
-        const entryEntity = getAncestorEntity(
-          ancestors,
-          parseInt(path[1] as string, 10) as number,
-        );
-        const isArgumentOfFuction = path[4] === "arguments";
-
-        const nodeDefinition: ASTNode | null = Array.isArray(ancestors[1])
-          ? ancestors[1].find((ancestor: ASTNode) => {
-              if ("name" in ancestor) {
-                return ancestor.name?.value === node.name;
-              }
-
-              return false;
-            })
-          : null;
-
-        const isNodeDefinitionInput =
-          nodeDefinition?.kind === Kind.INPUT_OBJECT_TYPE_DEFINITION;
-
-        const isNodeDefinitionEnum =
-          nodeDefinition?.kind === Kind.ENUM_TYPE_DEFINITION;
-
-        const isNodeDefinitionScalar =
-          nodeDefinition?.kind === Kind.SCALAR_TYPE_DEFINITION;
-
-        if (
-          isImplementedInterface ||
-          entryEntity?.kind === Kind.INTERFACE_TYPE_DEFINITION
-        ) {
-          return createNullableType(
-            context.getModelType(node.name, false, false).toTypeReference(),
-          );
-        }
-
-        const isEntryEntityInput =
-          entryEntity?.kind === Kind.INPUT_OBJECT_TYPE_DEFINITION;
-
-        if (
-          (isEntryEntityInput && isArgumentOfFuction) ||
-          isNodeDefinitionInput
-        ) {
-          return createNullableType(
-            context.getModelType(node.name, true, false).toTypeReference(),
-          );
-        }
-
-        if (!isArgumentOfFuction) {
-          context.addEntityToImport(node.name);
-
-          return createNullableType(
-            context.getModelType(node.name, true, true).toTypeReference(),
-          );
-        }
-
-        if (
-          entryEntity?.kind === Kind.OBJECT_TYPE_DEFINITION &&
-          !isArgumentOfFuction
-        ) {
-          context.addEntityToImport(node.name);
-        }
-
-        if (
-          isArgumentOfFuction &&
-          (isNodeDefinitionEnum || isNodeDefinitionScalar)
-        ) {
-          return createNullableType(
-            context.getModelType(node.name, true, true).toTypeReference(),
-          );
-        }
-
-        return createNullableType(
-          context
-            .getModelType(
-              node.name,
-              true,
-              !isArgumentOfFuction && !isEntryEntityInput,
-            )
-            .toTypeReference(),
-        );
-      },
-    },
-    ListType: {
-      leave({ type }): ts.TypeNode {
-        return createNullableType(
-          factory.createTypeReferenceNode(
-            factory.createIdentifier("ReadonlyArray"),
-            [type as ts.TypeNode],
-          ),
-        );
-      },
-    },
-    NonNullType: {
-      leave({ type }): ts.TypeNode {
-        return createNonNullableType(type as ts.UnionTypeNode);
-      },
-    },
-
-    Name: {
-      leave(node) {
-        return node.value;
-      },
-    },
-
-    Directive: {
-      leave() {
-        return null;
-      },
-    },
-
-    InputValueDefinition: {
-      leave(node): ts.PropertySignature {
-        return factory.createPropertySignature(
-          undefined,
-          node.name,
-          undefined,
-          node.type,
-        );
-      },
-    },
-
-    InputObjectTypeDefinition: {
-      leave(node): ts.TypeAliasDeclaration {
-        return factory.createTypeAliasDeclaration(
-          undefined,
-          [factory.createModifier(ts.SyntaxKind.ExportKeyword)],
-          node.name,
-          undefined,
-          factory.createTypeLiteralNode(
-            ((node.fields as unknown) as ts.TypeElement[]) || [],
-          ),
-        );
-      },
-    },
-
-    UnionTypeDefinition: {
-      leave() {
-        return null;
-      },
-    },
-
-    EnumTypeDefinition: {
-      leave() {
-        return null;
-      },
-    },
-
-    InterfaceTypeDefinition: {
-      leave() {
-        return null;
-      },
-    },
-
-    DirectiveDefinition: {
-      leave() {
-        return null;
-      },
-    },
-
-    ScalarTypeDefinition: {
-      leave() {
-        return null;
-      },
+    resolveInfo: {
+      name: "info",
+      type: context.getResolveInfoType().toTypeReference(),
     },
   };
+
+  return factory.createTypeAliasDeclaration(
+    undefined,
+    [factory.createModifier(ts.SyntaxKind.ExportKeyword)],
+    factory.createIdentifier(field.name),
+    type.name === "Subscription"
+      ? [
+          factory.createTypeParameterDeclaration(
+            factory.createIdentifier("A"),
+            undefined,
+            factory.createKeywordTypeNode(ts.SyntaxKind.UnknownKeyword),
+          ),
+        ]
+      : undefined,
+    getResolverReturnType(
+      createTypeFromNode(context, field.type),
+      type.name,
+      resolverParametersDefinitions,
+    ),
+  );
+}
+
+function createInputObjectType(
+  context: TsCodegenContext,
+  type: InputObjectType,
+): ts.TypeAliasDeclaration {
+  return factory.createTypeAliasDeclaration(
+    undefined,
+    [factory.createModifier(ts.SyntaxKind.ExportKeyword)],
+    type.name,
+    undefined,
+    factory.createTypeLiteralNode(
+      type.fields.map(({ name, type }) =>
+        factory.createPropertySignature(
+          [factory.createModifier(ts.SyntaxKind.ReadonlyKeyword)],
+          factory.createIdentifier(name),
+          undefined,
+          createTypeFromNode(context, type),
+        ),
+      ),
+    ),
+  );
 }
