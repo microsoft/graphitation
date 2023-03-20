be817e2fe06c3ccb29c3561a90de48e7b5d56c66
Jakub Vejr <jakubvejr@microsoft.com>
2023-03-20 20:12:49 +0000

    graphql-codegne typescript operations plugin forked

---
+++ packages/graphql-codegen-typescript-operations/README.md
@@ -0,0 +1,1 @@
+# graphql-codegen-typescript-operations
---
+++ packages/graphql-codegen-typescript-operations/src/ts-operation-variables-to-object.ts
@@ -0,0 +1,11 @@
+import { TypeScriptOperationVariablesToObject as TSOperationVariablesToObject } from "@graphql-codegen/typescript";
+
+export class TypeScriptOperationVariablesToObject extends TSOperationVariablesToObject {
+  protected formatTypeString(
+    fieldType: string,
+    isNonNullType: boolean,
+    _hasDefaultValue: boolean,
+  ): string {
+    return fieldType;
+  }
+}
