412081bd1045547199bfab2bd27305ecc5170e58
Jyoti Prakash Sahoo <jpsahoo14@gmail.com>
2023-10-04 15:04:53 +0530

    Integrate Apollo inspector UI

--- packages/rempl-apollo-devtools/src/publisher/publishers/apollo-operations-tracker-publisher.ts
+++ packages/rempl-apollo-devtools/src/publisher/publishers/apollo-operations-tracker-publisher.ts
@@ -2,24 +2,24 @@ import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
 import { RemplWrapper } from "../rempl-wrapper";
 import {
   ApolloInspector,
-  IStopTracking,
-  IInspectorTrackingConfig,
   IDataView,
-  IVerboseOperation,
+  IApolloClientObject,
 } from "apollo-inspector";
-import { WrapperCallbackParams } from "../../types";
+import { ClientObject, WrapperCallbackParams } from "../../types";
+import { Subscription } from "rxjs";
+import { ICopyData } from "apollo-inspector-ui";
 
 export class ApolloOperationsTrackerPublisher {
   private remplWrapper: RemplWrapper;
   protected apolloPublisher;
-  protected stopTracking: IStopTracking | undefined;
+  protected apolloClients: ClientObject[];
   protected activeClient: ApolloClient<NormalizedCacheObject> | undefined;
-  protected isRecording: boolean;
-  protected lastDataReceived: Map<number, IVerboseOperation> | null;
+
+  private trackingSubscription: Subscription | null;
 
   constructor(remplWrapper: RemplWrapper) {
+    this.apolloClients = [];
     this.remplWrapper = remplWrapper;
-    this.isRecording = false;
     this.remplWrapper.subscribeToRemplStatus(
       "operations-tracker",
       this.setActiveClient.bind(this),
@@ -27,83 +27,88 @@ export class ApolloOperationsTrackerPublisher {
     );
     this.apolloPublisher = remplWrapper.publisher;
     this.attachMethodsToPublisher();
-    this.lastDataReceived = null;
+    this.trackingSubscription = null;
   }
 
   protected attachMethodsToPublisher() {
-    this.apolloPublisher.provide(
-      "startOperationsTracker",
-      (options: IInspectorTrackingConfig | undefined) => {
-        if (this.activeClient) {
-          this.isRecording = true;
-          const inspector = new ApolloInspector(this.activeClient);
-          if (options && Object.keys(options).length === 0) {
-            options = undefined;
-          }
-          this.stopTracking = inspector.startTracking(
-            options as unknown as IInspectorTrackingConfig,
-          );
-        }
-      },
-    );
+    this.apolloPublisher.provide("startOperationsTracker", (options: any) => {
+      this.trackingSubscription?.unsubscribe();
+      const apolloClients: IApolloClientObject[] = this.apolloClients.map(
+        (ac) =>
+          ({
+            client: ac.client as ApolloClient<NormalizedCacheObject>,
+            clientId: ac.clientId,
+          } as unknown as IApolloClientObject),
+      );
+      const inspector = new ApolloInspector(apolloClients);
+
+      const observable = inspector.startTrackingSubscription({
+        tracking: { trackVerboseOperations: true },
+        apolloClientIds: options.clientIds,
+        delayOperationsEmitByInMS: 500,
+      });
+
+      const subscription = observable.subscribe({
+        next: (data) => {
+          this.publishApolloOperations(data);
+        },
+        error: () => {
+          subscription.unsubscribe();
+        },
+        complete: () => {
+          subscription.unsubscribe();
+        },
+      });
+
+      this.trackingSubscription = subscription as any;
+    });
 
     this.apolloPublisher.provide("stopOperationsTracker", () => {
-      if (this.stopTracking) {
-        this.isRecording = false;
-        try {
-          const data = this.stopTracking?.();
-          this.publishApolloOperations(data);
-          const operationsMap = new Map();
-          data.verboseOperations?.forEach((op) => {
-            operationsMap.set(op.id, op);
-          });
-          this.lastDataReceived = operationsMap;
-        } catch (error) {
-          // publish error to subscriber to show error UX
-          this.publishApolloOperations({
-            error: JSON.stringify(error),
-            message: "Something went wrong",
-          } as any);
-          throw error;
-        }
-
-        this.stopTracking = undefined;
+      try {
+        this.trackingSubscription?.unsubscribe();
+      } catch (error) {
+        // publish error to subscriber to show error UX
+        this.publishApolloOperations({
+          error: JSON.stringify(error),
+          message: "Something went wrong",
+        } as any);
       }
     });
 
     this.apolloPublisher.provide(
       "copyOperationsData",
-      (ids: number[] | undefined) => {
-        if (ids && ids.length === 1 && ids[0] === -1) {
-          const stringified = JSON.stringify(
-            (this.activeClient?.cache as any).data.data,
-          );
-          window.navigator.clipboard.writeText(stringified);
-          return;
-        }
-        if (this.lastDataReceived) {
-          const copiedOperations: IVerboseOperation[] = [];
-          ids?.forEach((id) => {
-            if (this.lastDataReceived?.has(id)) {
-              copiedOperations.push(
-                this.lastDataReceived.get(id) as IVerboseOperation,
-              );
-            }
-          });
-
-          copiedOperations.sort((a, b) => a.id - b.id);
-          const stringified = JSON.stringify(copiedOperations);
-          window.navigator.clipboard.writeText(stringified);
-        }
+      (data: { data: ICopyData }) => {
+        try {
+          if (data.data.clientId) {
+            const apolloClient = this.apolloClients.find(
+              (ac) => ac.clientId === data.data.clientId,
+            );
+            window.navigator.clipboard.writeText(
+              JSON.stringify((apolloClient?.client.cache as any).data.data),
+            );
+          } else {
+            const copiedData = JSON.stringify(data.data.operations);
+            window.navigator.clipboard.writeText(copiedData);
+          }
+        } catch {}
       },
     );
+
+    this.apolloPublisher.provide("getApolloClientsIds", () => {
+      const clientIds = this.apolloClients.map((client) => client.clientId);
+      this.apolloPublisher.ns("apollo-operations-tracker").publish(clientIds);
+    });
   }
 
   protected publishApolloOperations(data: IDataView) {
     this.apolloPublisher.ns("apollo-operations-tracker").publish(data);
   }
 
-  private setActiveClient({ activeClient }: WrapperCallbackParams) {
+  private setActiveClient({
+    activeClient,
+    clientObjects,
+  }: WrapperCallbackParams) {
     this.activeClient = activeClient?.client;
+    this.apolloClients = clientObjects;
   }
 }
--- packages/rempl-apollo-devtools/src/subscriber/apollo-operations-tracker/operations-tracker-container.tsx
+++ packages/rempl-apollo-devtools/src/subscriber/apollo-operations-tracker/operations-tracker-container.tsx
@@ -1,186 +1,109 @@
 import { remplSubscriber } from "../rempl";
-import React, { useState, useEffect, useCallback } from "react";
+import * as React from "react";
 import { IDataView } from "apollo-inspector";
-import { mergeClasses, Spinner, Title2 } from "@fluentui/react-components";
-import { OperationsTrackerBody } from "./operations-tracker-body/operations-tracker-body";
-import { useStyles } from "./operations-tracker-container-styles";
-import { OperationsTrackerHeader } from "./operations-tracker-header/operations-tracker-header";
-import {
-  IError,
-  ILoader,
-  IUseMainSlotParams,
-  IUseMainSlotService,
-} from "./operations-tracker-container.interface";
-import { ErrorBoundary } from "./operation-tracker-error-boundary";
-import {
-  getInitialState,
-  OperationReducerActionEnum,
-  reducers,
-} from "./operations-tracker-container-helper";
+import { OperationsTrackerContainer as Main } from "apollo-inspector-ui";
+import { Observable } from "rxjs";
+import { UnsubscribeFn } from "rempl/lib/utils";
+import { CopyType } from "apollo-inspector-ui";
 
 export const OperationsTrackerContainer = () => {
-  const [openDescription, setOpenDescription] = useState<boolean>(false);
-  const [apollOperationsData, setApolloOperationsData] =
-    useState<IDataView | null>(null);
-  const [loader, setLoader] = React.useState<ILoader>({
-    message: "",
-    loading: false,
-  });
-  const [error, setError] = React.useState<IError | null>(null);
-  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [clientObjects, setClientObjects] = React.useState<string[]>([]);
 
-  const [operationsState, dispatchOperationsState] = React.useReducer(
-    reducers,
-    getInitialState(),
-  );
+  const { onRecordStart, onRecordStop }: IUseOnRecordPayload =
+    useOnRecord(clientObjects);
 
-  const classes = useStyles();
-  useSubscribeToPublisher(setError, setApolloOperationsData, setLoader);
+  useSubscribeToPublisher({ setClientObjects });
 
-  const toggleRecording = useToggleRecording(
-    setIsRecording,
-    setApolloOperationsData,
-    setLoader,
-    setError,
-  );
-  React.useMemo(() => {
-    return null;
-  }, [operationsState]);
-
-  const clearApolloOperations = useCallback(() => {
-    setApolloOperationsData(null);
-  }, [setApolloOperationsData]);
-
-  useEffect(() => {
-    return () => {
-      remplSubscriber.callRemote("stopOperationsTracker", {});
-    };
-  }, [remplSubscriber]);
-
-  const mainSlot = useMainSlot(
-    {
-      error,
-      loader,
-      apollOperationsData,
-      operationsState,
-      dispatchOperationsState,
-    },
-    { classes },
-  );
-
-  const setSearchText = React.useCallback(
-    (text) => {
-      dispatchOperationsState({
-        type: OperationReducerActionEnum.UpdateSearchText,
-        value: text,
-      });
-    },
-    [dispatchOperationsState],
-  );
+  const onCopy = useOnCopy();
 
   return (
-    <ErrorBoundary>
-      <div className={classes.root}>
-        <div
-          className={mergeClasses(
-            classes.innerContainer,
-            openDescription && classes.innerContainerDescription,
-          )}
-        >
-          <OperationsTrackerHeader
-            isRecording={isRecording}
-            openDescription={openDescription}
-            setOpenDescription={setOpenDescription}
-            toggleRecording={toggleRecording}
-            setSearchText={setSearchText}
-            operationsState={operationsState}
-            apollOperationsData={apollOperationsData}
-            clearApolloOperations={clearApolloOperations}
-            showClear={!!apollOperationsData?.verboseOperations}
-          />
-          {mainSlot}
-        </div>
-      </div>
-    </ErrorBoundary>
+    <Main
+      onRecordStart={onRecordStart}
+      onRecordStop={onRecordStop}
+      apolloClientIds={clientObjects}
+      onCopy={onCopy}
+    ></Main>
   );
 };
 
-const useMainSlot = (
-  {
-    apollOperationsData,
-    error,
-    loader,
-    dispatchOperationsState,
-    operationsState,
-  }: IUseMainSlotParams,
-  { classes }: IUseMainSlotService,
-) => {
-  if (error) {
-    return (
-      <div className={classes.centerDiv}>
-        <Title2>{error.message}</Title2>
-      </div>
-    );
-  }
-  if (loader.loading) {
-    return (
-      <div className={classes.centerDiv}>
-        <Spinner labelPosition="below" label={loader.message} />
-      </div>
-    );
-  }
-
-  return (
-    <OperationsTrackerBody
-      dispatchOperationsState={dispatchOperationsState}
-      data={apollOperationsData}
-      operationsState={operationsState}
-    />
-  );
-};
-
-const useToggleRecording = (
-  setIsRecording: React.Dispatch<React.SetStateAction<boolean>>,
-  setApolloOperationsData: React.Dispatch<
-    React.SetStateAction<IDataView | null>
-  >,
-  setLoader: React.Dispatch<React.SetStateAction<ILoader>>,
-  setError: React.Dispatch<React.SetStateAction<IError | null>>,
-) =>
-  useCallback(() => {
-    setIsRecording?.((isRecording) => {
-      if (!isRecording) {
-        remplSubscriber.callRemote("startOperationsTracker");
-        setApolloOperationsData(null);
-        setLoader({ message: "Recording operations", loading: true });
-        setError(null);
-      } else {
-        remplSubscriber.callRemote("stopOperationsTracker", {});
-        setLoader({ message: "Processing operations", loading: true });
-      }
-      return !isRecording;
-    });
-  }, [setIsRecording]);
-
-const useSubscribeToPublisher = (
-  setError: React.Dispatch<React.SetStateAction<IError | null>>,
-  setApolloOperationsData: React.Dispatch<
-    React.SetStateAction<IDataView | null>
-  >,
-  setLoader: React.Dispatch<React.SetStateAction<ILoader>>,
-) => {
-  useEffect(() => {
-    remplSubscriber
+const useSubscribeToPublisher = ({
+  setClientObjects,
+}: {
+  setClientObjects: React.Dispatch<React.SetStateAction<string[]>>;
+}) => {
+  React.useEffect(() => {
+    remplSubscriber.callRemote("getApolloClientsIds");
+    const unsubscribe = remplSubscriber
       .ns("apollo-operations-tracker")
       .subscribe((data: IDataView) => {
-        if (data && (data as any).message) {
-          const typedData = data as any;
-          setError({ error: typedData.error, message: typedData.message });
-          setLoader({ message: "", loading: false });
-          return;
+        if (Array.isArray(data)) {
+          setClientObjects(data);
+          unsubscribe();
         }
-        setApolloOperationsData(data);
-        setLoader({ message: "", loading: false });
       });
-  }, []);
+  }, [setClientObjects]);
 };
+
+interface IUseOnRecordPayload {
+  onRecordStart: any;
+  onRecordStop: () => void;
+}
+const useOnRecord = (clientObjects: string[]): IUseOnRecordPayload => {
+  const unsubscribeRef = React.useRef<UnsubscribeFn>();
+  const onRecordStart: any = useOnRecordStart(unsubscribeRef, clientObjects);
+  const onRecordStop = useOnRecordStop(unsubscribeRef);
+  React.useEffect(() => () => unsubscribeRef.current?.(), []);
+  return { onRecordStart, onRecordStop };
+};
+
+const useOnCopy = () =>
+  React.useCallback((copyType: CopyType, data: any) => {
+    switch (copyType) {
+      case CopyType.AllOperations:
+      case CopyType.CheckedOperations:
+      case CopyType.FilteredOperations:
+      case CopyType.SelectedOperation:
+      case CopyType.WholeApolloCache:
+        {
+          remplSubscriber.callRemote("copyOperationsData", { data });
+        }
+
+        break;
+    }
+  }, []);
+
+const useOnRecordStop = (
+  unsubscribeRef: React.MutableRefObject<UnsubscribeFn | undefined>,
+) =>
+  React.useCallback(() => {
+    unsubscribeRef.current?.();
+    remplSubscriber.callRemote("stopOperationsTracker", {});
+  }, []);
+
+const useOnRecordStart = (
+  unsubscribeRef: React.MutableRefObject<UnsubscribeFn | undefined>,
+  clientObjects: string[],
+) =>
+  React.useCallback(
+    (selectedApolloClientIds: string[]) => {
+      return new Observable<IDataView>((observer) => {
+        remplSubscriber.callRemote("startOperationsTracker", {
+          clientIds: selectedApolloClientIds,
+        });
+        if (unsubscribeRef.current) {
+          unsubscribeRef.current();
+        }
+        let skipValue = true;
+        unsubscribeRef.current = remplSubscriber
+          .ns("apollo-operations-tracker")
+          .subscribe((data: IDataView) => {
+            if (!Array.isArray(data) && !skipValue) {
+              observer.next(data);
+            } else {
+              skipValue = false;
+            }
+          });
+      });
+    },
+    [clientObjects],
+  );
