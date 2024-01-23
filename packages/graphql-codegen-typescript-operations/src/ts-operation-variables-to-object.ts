8a5e925d50980ae55bcf075eb549a021585f5e2b
Jakub Vejr <jakubvejr@microsoft.com>
2024-01-23 19:13:30 +0000

    Exact/Maybe and Basic scalars are inlined into documents

--- packages/graphql-codegen-typescript-operations/src/ts-operation-variables-to-object.ts
+++ packages/graphql-codegen-typescript-operations/src/ts-operation-variables-to-object.ts
@@ -1,5 +1,6 @@
 import { TypeScriptOperationVariablesToObject as TSOperationVariablesToObject } from "@graphql-codegen/typescript";
 
+const BASIC_SCALARS = ["ID", "Int", "Float", "String", "Boolean"];
 export class TypeScriptOperationVariablesToObject extends TSOperationVariablesToObject {
   protected formatTypeString(
     fieldType: string,
@@ -7,5 +8,15 @@ export class TypeScriptOperationVariablesToObject extends TSOperationVariablesTo
     _hasDefaultValue: boolean,
   ): string {
     return fieldType;
+  }
+  protected getScalar(name: string): string {
+    if (BASIC_SCALARS.includes(name)) {
+      return this._scalars[name];
+    }
+    const prefix = this._namespacedImportName
+      ? `${this._namespacedImportName}.`
+      : "";
+
+    return `${prefix}Scalars['${name}']`;
   }
 }
