041bbcd7438f7a649ee5629515a3c44a6a879b3b
Dmitrii Samsonov <dsamsonov@microsoft.com>
2024-06-04 16:47:08 +0200

    add ban directives rule

--- packages/graphql-eslint-rules/src/index.ts
+++ packages/graphql-eslint-rules/src/index.ts
@@ -4,3 +4,5 @@ import operationNamingConventions from "./operation-naming-conventions";
 export { operationNamingConventions as operationNamingConventionRule };
 import fragmentNamingConventions from "./fragment-naming-conventions";
 export { fragmentNamingConventions as fragmentNamingConventionRule };
+import banDirectives from "./ban-directives";
+export { banDirectives as banDirectivesRule };
