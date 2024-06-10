381556fae0606cf4e312b7a26d59eec9a5636fa3
Sergey Stoyan <sergeystoyan@microsoft.com>
2024-06-10 10:32:24 +0200

    Optimize printPathArray function:
     - remove beginning dot
     - use += instead of map/join to speed up strings merging

--- packages/supermassive/src/jsutils/printPathArray.ts
+++ packages/supermassive/src/jsutils/printPathArray.ts
@@ -2,9 +2,16 @@
  * Build a string describing the path.
  */
 export function printPathArray(path: ReadonlyArray<string | number>): string {
-  return path
-    .map((key) =>
-      typeof key === "number" ? "[" + key.toString() + "]" : "." + key,
-    )
-    .join("");
+  let result = "";
+  for (let i = 0; i < path.length; i++) {
+    if (typeof path[i] === "number") {
+      result += `[${path[i].toString()}]`;
+    } else {
+      if (i > 0) {
+        result += ".";
+      }
+      result += path[i];
+    }
+  }
+  return result;
 }
