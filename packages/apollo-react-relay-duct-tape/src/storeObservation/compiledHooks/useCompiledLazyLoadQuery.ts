1a0e2a85fd65d49ca02b37981ec03bfbd920a301
pavelglac <pavelglac@gmail.com>
2025-03-21 10:22:36 +0100

    rename method

--- packages/apollo-react-relay-duct-tape/src/storeObservation/compiledHooks/useCompiledLazyLoadQuery.ts
+++ packages/apollo-react-relay-duct-tape/src/storeObservation/compiledHooks/useCompiledLazyLoadQuery.ts
@@ -17,7 +17,7 @@ class ExecutionQueryHandler {
   public status: [pending: boolean, error?: Error];
   private querySubscription?: ZenObservable.Subscription;
 
-  constructor(private onComplete: () => void) {
+  constructor(private onDataReceived: () => void) {
     this.status = [true, undefined];
   }
 
@@ -46,7 +46,7 @@ class ExecutionQueryHandler {
     if (!loading) {
       this.dispose();
     }
-    this.onComplete();
+    this.onDataReceived();
   }
 
   public subscribe(observable: ObservableQuery) {
