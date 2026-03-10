f489426da39a277befd4ea4c765dd483b3409487
vrazuvaev <vrazuvaev@microsoft.com_msteamsmdb>
2026-03-10 12:28:17 +0100

    fix(apollo-react-relay-duct-tape-compiler): connection directive works with both "filters" and "filter" argument variants

--- packages/apollo-react-relay-duct-tape-compiler/src/compilerTransforms/emitApolloClientConnectionTransform.ts
+++ packages/apollo-react-relay-duct-tape-compiler/src/compilerTransforms/emitApolloClientConnectionTransform.ts
@@ -6,6 +6,10 @@ import { visit } from "relay-compiler/lib/core/IRVisitor";
 // TODO: Add typings for this
 const SchemaUtils = require("relay-compiler/lib/core/SchemaUtils");
 
+type Args = {
+  renameFiltersArgument: boolean;
+};
+
 /**
  * Finds connection client-handles that relay-compiler emits and replaces
  * them with a version of the `@connection` directive that Apollo Client uses;
@@ -15,83 +19,85 @@ const SchemaUtils = require("relay-compiler/lib/core/SchemaUtils");
  * @param wrappedConnectionTransform
  *   relay-compiler's connection client-handle transform
  */
-export function emitApolloClientConnectionTransform(
-  wrappedConnectionTransform: IRTransform,
-): IRTransform {
-  const connectionTransformWrapper: IRTransform = (context) => {
-    const schema = context.getSchema();
-    let nextContext = wrappedConnectionTransform(context);
+export function emitApolloClientConnectionTransform({
+  renameFiltersArgument,
+}: Args): (wrapped: IRTransform) => IRTransform {
+  return (wrappedConnectionTransform: IRTransform) => {
+    const connectionTransformWrapper: IRTransform = (context) => {
+      const schema = context.getSchema();
+      let nextContext = wrappedConnectionTransform(context);
 
-    // Replaces the field handle created by the wrapped connection transform
-    // with the @connection directive that Apollo Client expects
-    nextContext.forEachDocument((document) => {
-      const nextDocument = visit(document, {
-        LinkedField(linkedFieldNode) {
-          const connectionHandle = linkedFieldNode.handles?.find(
-            (handle) => handle.name === "connection",
-          );
-          if (connectionHandle) {
-            const args: Argument[] = [
-              {
-                kind: "Argument",
-                name: "key",
-                type: SchemaUtils.getNonNullStringInput(schema),
-                value: {
-                  kind: "Literal",
-                  value: connectionHandle.key,
-                  loc: { kind: "Generated" },
-                },
-                loc: { kind: "Generated" },
-                metadata: undefined,
-              },
-            ];
-            if (connectionHandle.filters) {
-              args.push({
-                kind: "Argument",
-                name: "filter",
-                type: schema.assertInputType(
-                  schema.expectTypeFromString("[String!]"),
-                ),
-                value: {
-                  kind: "ListValue",
-                  items: connectionHandle.filters.map<ArgumentValue>(
-                    (filter) => ({
-                      kind: "Literal",
-                      value: filter,
-                      loc: { kind: "Generated" },
-                    }),
-                  ),
-                  loc: { kind: "Generated" },
-                  metadata: undefined,
-                },
-                loc: { kind: "Generated" },
-                metadata: undefined,
-              });
-            }
-            const nextLinkedFieldNode: LinkedField = {
-              ...linkedFieldNode,
-              handles: linkedFieldNode.handles?.filter(
-                (handle) => handle.name !== "connection",
-              ),
-              directives: [
-                ...linkedFieldNode.directives,
+      // Replaces the field handle created by the wrapped connection transform
+      // with the @connection directive that Apollo Client expects
+      nextContext.forEachDocument((document) => {
+        const nextDocument = visit(document, {
+          LinkedField(linkedFieldNode) {
+            const connectionHandle = linkedFieldNode.handles?.find(
+              (handle) => handle.name === "connection",
+            );
+            if (connectionHandle) {
+              const args: Argument[] = [
                 {
-                  kind: "Directive",
-                  name: "connection",
-                  args,
+                  kind: "Argument",
+                  name: "key",
+                  type: SchemaUtils.getNonNullStringInput(schema),
+                  value: {
+                    kind: "Literal",
+                    value: connectionHandle.key,
+                    loc: { kind: "Generated" },
+                  },
                   loc: { kind: "Generated" },
                   metadata: undefined,
                 },
-              ],
-            };
-            return nextLinkedFieldNode;
-          }
-        },
+              ];
+              if (connectionHandle.filters) {
+                args.push({
+                  kind: "Argument",
+                  name: renameFiltersArgument ? "filter" : "filters",
+                  type: schema.assertInputType(
+                    schema.expectTypeFromString("[String!]"),
+                  ),
+                  value: {
+                    kind: "ListValue",
+                    items: connectionHandle.filters.map<ArgumentValue>(
+                      (filter) => ({
+                        kind: "Literal",
+                        value: filter,
+                        loc: { kind: "Generated" },
+                      }),
+                    ),
+                    loc: { kind: "Generated" },
+                    metadata: undefined,
+                  },
+                  loc: { kind: "Generated" },
+                  metadata: undefined,
+                });
+              }
+              const nextLinkedFieldNode: LinkedField = {
+                ...linkedFieldNode,
+                handles: linkedFieldNode.handles?.filter(
+                  (handle) => handle.name !== "connection",
+                ),
+                directives: [
+                  ...linkedFieldNode.directives,
+                  {
+                    kind: "Directive",
+                    name: "connection",
+                    args,
+                    loc: { kind: "Generated" },
+                    metadata: undefined,
+                  },
+                ],
+              };
+              return nextLinkedFieldNode;
+            }
+          },
+        });
+        nextContext = nextContext.replace(nextDocument);
       });
-      nextContext = nextContext.replace(nextDocument);
-    });
 
-    return nextContext;
+      return nextContext;
+    };
+    return connectionTransformWrapper;
   };
-  return connectionTransformWrapper;
 }
